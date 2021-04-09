3226c4625a72a8574466c124025babb37d3ee4a5
dawright22 <53329137+dawright22@users.noreply.github.com>
2021-04-10 09:50:48 +1000

    Update helm-init.sh

--- app_stack/app_GKE/tiller/helm-init.sh
+++ app_stack/app_GKE/tiller/helm-init.sh
@@ -6,4 +6,4 @@ kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 
 # Let Helm deploy and configure the Tiller service.
-helm init --service-account tiller
+helm2 init --service-account tiller
