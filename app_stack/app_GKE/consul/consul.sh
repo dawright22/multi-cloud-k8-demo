66a4fc2a4587d9b1ea28cf887be008dbd38d7591
dawright22 <53329137+dawright22@users.noreply.github.com>
2021-04-10 09:52:19 +1000

    Update consul.sh

--- app_stack/app_GKE/consul/consul.sh
+++ app_stack/app_GKE/consul/consul.sh
@@ -3,7 +3,7 @@ set -v
 
 echo "Installing Consul from Helm chart repo..."
 git clone https://github.com/hashicorp/consul-helm.git
-helm install --name=consul -f ./values.yaml ./consul-helm
+helm2 install --name=consul -f ./values.yaml ./consul-helm
 
 sleep 10s
 
